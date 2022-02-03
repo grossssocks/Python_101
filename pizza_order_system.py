9246a0c833c22f0be90eb826409c6932a0a6f3b5
Sujita <92402128+grossssocks@users.noreply.github.com>
2022-02-03 10:24:43 +0530

    Update pizza_order_system.py

--- pizza_order_system.py
+++ pizza_order_system.py
@@ -1,3 +1,10 @@
+#Small Pizza: $15
+#Medium Pizza: $20
+#Large Pizza: $25
+#Pepperoni for Small Pizza: +$2
+#Pepperoni for Medium or Large Pizza: +$3
+#Extra cheese for any size pizza: + $
+
 print("Welcome to Python Pizza Deliveries!")
 size = input("What size pizza do you want? S, M, or L ")
 add_pepperoni = input("Do you want pepperoni? Y or N ")
